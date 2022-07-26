b8aeb2387a72b20fa7ffffd8011201f67a7ccbff
赵泽林(实习) <zhaozelin@megvii.com>
2022-07-26 20:54:20 +0800

    clean: remove ipynb and move it to .py

--- data_preprocess/load_data.py
+++ data_preprocess/load_data.py
@@ -1,35 +1,15 @@
 import tensorflow as tf
+import numpy as np
 import datetime, os
-from google.colab import drive
 from base64 import decodestring
-
-drive.mount("/content/drive")
 import cv2
+import os
 import matplotlib.pyplot as plt
-
-demo_data_p = "data/v1.0_waymo_block_nerf_mission_bay_train.tfrecord-00000-of-01063"
-filenames = [demo_data_p]
-dataset = tf.data.TFRecordDataset(filenames[0])
-
-for raw_record in dataset.take(10):
-    print(repr(raw_record))
-
-pdb.set_trace()
-
-# Read the data back out.
-def decode_fn(record_bytes):
-    return tf.io.parse_single_example(
-        # Data
-        record_bytes,
-        # Schema
-        {
-            "image_hash": tf.io.FixedLenFeature([], dtype=tf.float32),
-            "cam_idx": tf.io.FixedLenFeature([], dtype=tf.float32),
-        },
-    )
-
-
-demo_data_p = "v1.0_waymo_block_nerf_mission_bay_train.tfrecord-00011-of-01063"
+import json
+import torch
+demo_data_p = "/content/drive/MyDrive/blocknerf/v1.0_waymo_block_nerf_mission_bay_train.tfrecord-00002-of-01063"
+train_meta, val_meta = {}, {}
+train_or_val = "train" if "train" in demo_data_p else "val"
 filenames = [demo_data_p]
 dataset = tf.data.TFRecordDataset(
     filenames[0],
@@ -47,21 +27,11 @@ def decode_fn(record_bytes):
             "equivalent_exposure": tf.io.FixedLenFeature([], dtype=tf.float32),
             "height": tf.io.FixedLenFeature([], dtype=tf.int64),
             "width": tf.io.FixedLenFeature([], dtype=tf.int64),
-            "image": tf.io.FixedLenFeature([], dtype=tf.float32),
-            "ray_origins": tf.io.FixedLenFeature([], dtype=tf.float32),
-            "ray_dirs": tf.io.FixedLenFeature([], dtype=tf.float32),
-            "intrinsics": tf.io.FixedLenFeature([], dtype=tf.float32),
+            "image": tf.io.FixedLenFeature([], dtype=tf.string),
+            "ray_origins": tf.io.VarLenFeature(tf.float32),
+            "ray_dirs": tf.io.VarLenFeature(tf.float32),
+            "intrinsics": tf.io.VarLenFeature(tf.float32),
         },
     )
 
-
 dataset_map = dataset.map(decode_fn)
-
-i = 0
-for batch in dataset_map:
-    i += 1
-    imagestr = batch["image"]
-    image = tf.io.decode_png(imagestr, channels=0, dtype=tf.dtypes.uint8, name=None)
-    print(image.shape)
-    plt.imshow(image)
-    break
