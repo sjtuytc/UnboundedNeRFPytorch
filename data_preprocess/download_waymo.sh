5680898baf2409f7500d78fcb73af657105a93ff
Zelin Zhao <sjtuytc@gmail.com>
2022-09-10 22:35:56 +0800

    feat: update zip file location

--- data_preprocess/download_waymo.sh
+++ data_preprocess/download_waymo.sh
@@ -1,2 +1,2 @@
-gdown --id 1Lcc6MF35EnXGyUy0UZPkUx7SfeLsv8u9
+gdown --id 1paqYra8mbryVsoXAiJCImXXE0qFGxC0N # pytorch_waymo_dataset, https://drive.google.com/file/d/1paqYra8mbryVsoXAiJCImXXE0qFGxC0N/view?usp=sharing
 gdown --id 1O7uzcPBQHNAcmAcmcS6TRbLqiIDE3D0y # ckpts, will be updated at next time
