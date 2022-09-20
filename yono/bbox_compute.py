fix: fix height weight calculation

yono/bbox_compute.py
@@ -3,6 +3,7 @@ import numpy as np
 from yono import utils, dvgo
 import time
 from yono.load_everything import load_existing_model
+from tqdm import tqdm
 import pdb
 
 
@@ -10,7 +11,7 @@ def _compute_bbox_by_cam_frustrm_unbounded(cfg, HW, Ks, poses, i_train, near_cli
     # Find a tightest cube that cover all camera centers
     xyz_min = torch.Tensor([np.inf, np.inf, np.inf])
     xyz_max = -xyz_min
-    for (H, W), K, c2w in zip(HW[i_train], Ks[i_train], poses[i_train]):
+    for (H, W), K, c2w in tqdm(zip(HW[i_train], Ks[i_train], poses[i_train]), total=len(HW[i_train])):
         rays_o, rays_d, viewdirs = dvgo.get_rays_of_a_view(
                 H=H, W=W, K=K, c2w=c2w,
                 ndc=cfg.data.ndc, inverse_y=cfg.data.inverse_y,

yono/load_waymo.py
@@ -145,8 +145,9 @@ def load_waymo(args, basedir, rerotate=True):
     #     poses, render_poses = rerotate_poses(poses, render_poses)
 
     # render_poses = torch.Tensor(render_poses)
-    H, W = metadata['train']['height'][0], metadata['train']['width'][0]
-    HW = np.array([[H, W] for i in imgs])
+    train_HW = np.array([[metadata['train']['height'][i], metadata['train']['width'][i]] for i in range(len(metadata['train']['height']))]).tolist()
+    test_HW = np.array([[metadata['test']['height'][i], metadata['test']['width'][i]] for i in range(len(metadata['test']['height']))]).tolist()
+    HW = np.array(train_HW + test_HW)
     if args.sample_num > 0:
         i_split[0] = i_split[0][:args.sample_num]
     return imgs, poses, None, HW, all_K, i_split